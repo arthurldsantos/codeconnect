426a296f45448fb799cd1c5b4133001a610d2b03
Arthur Santos <arthurldsantos@gmail.com>
2024-12-16 13:19:00 -0300

    implementar funcionalidade para ler o arquivo de imagem

--- scripts.js
+++ scripts.js
@@ -4,3 +4,18 @@ const inputUpload = document.getElementById('imagem-upload');
 uploadBtn.addEventListener('click', () => {
     inputUpload.click();
 });
+
+function lerConteudoDoArquivo (arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name });
+        };
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`);
+        };
+
+        leitor.readAsDataURL(arquivo);
+    });
+}
